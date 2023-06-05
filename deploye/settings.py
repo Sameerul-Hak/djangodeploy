05692f8b5d8fa8cb64a9e63e46453590175382ef
Sameerul-Hak <sameerulhakofficial@gmail.com>
2023-06-05 12:34:59 +0530

    added image and css

--- deploye/settings.py
+++ deploye/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nrmjihev-af7qtn+l22p1-ccst*)2xbbz$f-0s8q)p%(=c5v*a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1",".vercel.app"]
 
 
 # Application definition
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    BASE_DIR/'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
