51965db8f8e3b0aa352d0f6c69ae03c2475c29ff
Sameerul-Hak <sameerulhakofficial@gmail.com>
2023-06-08 15:28:19 +0530

    clever cloud sql

--- deploye/settings.py
+++ deploye/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +77,15 @@ WSGI_APPLICATION = 'deploye.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "bxugmobeqxyibltpw4km",
+        'USER': "u2ba71jz2494ocsa",
+        'PASSWORD': "WVGcYCdw3vO0jRuV22PM",
+        'HOST': "bxugmobeqxyibltpw4km-mysql.services.clever-cloud.com",
+        'PORT':"3306",
+      
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- dev/views.py
+++ dev/views.py
@@ -9,16 +9,7 @@ def home(request):
 
 def particularpost(request,pk):
     postpar=Post.objects.get(id=pk)
-    if request.method=="POST":
-        comment=Form(request.POST)
-        if comment.is_valid():
-            comm=comment.save(commit=False)
-            comm.user=request.user
-            comm.post=postpar
-            comm.save()
-            
-            return redirect('home')
-    comment=Form()
     comments=Comments.objects.filter(post=postpar)
-    return render(request,"dev/particularpost.html",{"post":postpar,"comments":comments,'form':comment})
+    print(comments)
+    return render(request,"dev/particularpost.html",{"post":postpar,"comments":comments})
 
